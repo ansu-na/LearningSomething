fe5590882e6ad29ab545963b0320e564fe85b5a5
ansu-na <51852516+ansu-na@users.noreply.github.com>
2022-04-08 08:51:09 +0900

    Create luke_I_am_your.py

---
+++ coding_problems/luke_I_am_your.py
@@ -0,0 +1,10 @@
+# https://edabit.com/challenge/8pDH2SRutPoaQghgc
+# I used dict and format to solve the problem
+# And I'm realy happy to see that most voted solution seems same with my one!
+
+def relation_to_luke(name):
+	relations = {"Darth Vader" :	"father",
+  	"Leia" :	"sister",
+  	"Han" :	"brother in law",
+  	"R2D2" : "droid" };
+	return "Luke, I am your {0}.".format(relations[name]);
