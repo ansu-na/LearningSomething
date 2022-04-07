b424953414e5f9e7814f4d72231529783d2670fd
ansu-na <51852516+ansu-na@users.noreply.github.com>
2022-04-08 08:52:42 +0900

    Update luke_I_am_your.py

--- coding_problems/luke_I_am_your.py
+++ coding_problems/luke_I_am_your.py
@@ -3,8 +3,8 @@
 # And I'm realy happy to see that most voted solution seems same with my one!
 
 def relation_to_luke(name):
-	relations = {"Darth Vader" :	"father",
-  	"Leia" :	"sister",
-  	"Han" :	"brother in law",
-  	"R2D2" : "droid" };
-	return "Luke, I am your {0}.".format(relations[name]);
+    relations = {"Darth Vader" : "father",
+    "Leia" : "sister",
+    "Han" :	"brother in law",
+    "R2D2" : "droid" };
+    return "Luke, I am your {0}.".format(relations[name]);
