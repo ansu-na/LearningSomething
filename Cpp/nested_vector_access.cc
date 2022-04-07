1f7071639d7540a7e8da8e7c70362ec83ef7dd40
ansu-na <51852516+ansu-na@users.noreply.github.com>
2022-04-08 08:39:27 +0900

    Update nested_vector_access.cc

--- Cpp/nested_vector_access.cc
+++ Cpp/nested_vector_access.cc
@@ -6,7 +6,7 @@
 int main()
 {
     // 3 * 3 int matrix
-    std::vector<std::vector<int>> matrix = { {1 , 2, 3} , {4, 5, 6}, {7, 8, 9} };
+    std::vector<std::vector<int>> matrix = { {1, 2, 3} , {4, 5, 6}, {7, 8, 9} };
     std::cout << matrix[0][0] << std::endl;
     std::cout << matrix[1][1] << std::endl;
     std::cout << matrix[2][2] << std::endl;
