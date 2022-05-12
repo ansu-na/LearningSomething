7fe1d884f552e807ec199c3799cb293b123ae365
ansu-na <51852516+ansu-na@users.noreply.github.com>
2022-05-12 10:37:23 +0900

    Update stand_in_line.py

--- coding_problems/python/stand_in_line.py
+++ coding_problems/python/stand_in_line.py
@@ -6,3 +6,8 @@ def next_in_line(lst, num):
 	lst.pop(0)
 	lst.append(num)
 	return lst
+
+# solution from zatoichi49
+# https://edabit.com/user/tAF9Gf6PiGogbZuWs
+def next_in_line(lst, num):
+	return lst[1:] + [num] if lst else "No list has been selected"
